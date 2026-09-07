9b7162b945fbde426860d8deed4c8f7be868567e
Valentin De Pascale <valentindp9@gmail.com>
2026-09-07 19:49:36 -0300

    Update import path for ej16-sueldos.js

--- src/tests/ej16-sueldos.test.ts
+++ src/tests/ej16-sueldos.test.ts
@@ -5,7 +5,8 @@ import {
     EmpleadoPorHora,
     EmpleadoComision,
     calcularSueldos,
-} from "./ej16-sueldos.js";
+} from "../parte 3/ej16-sueldos.js";
+
 
 describe("Ejercicio 16 - EmpleadoFijo", () => {
     it("cobra el sueldo mensual fijo", () => {
